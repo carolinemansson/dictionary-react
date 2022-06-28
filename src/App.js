1b4ec45aa4aef887ca690880eafb87d45760bdae
carolinemansson <97236288+carolinemansson@users.noreply.github.com>
2022-06-28 15:51:33 +0200

    Added new file, Dictionary.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Dictionary from "./Dictionary";
+
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
         <footer className="text-center">Coded by Caroline Månsson</footer>
       </div>
     </div>
