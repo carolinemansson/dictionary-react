2bac7728f7706ee9f38ed5d78cb9fbce878f443f
carolinemansson <97236288+carolinemansson@users.noreply.github.com>
2022-07-05 14:42:42 +0200

    Added padding + emoji

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"><span className="dino">DINO</span> Dictionary</header>
+        <header className="App-header">
+          <span className="dino">DINO</span> Dictionary 🦖
+        </header>
         <main>
           <Dictionary defaultKeyword="dinosaur" />
         </main>
-        <footer className="App-footer"><small>Coded by Caroline Månsson</small></footer>
+        <footer className="App-footer">
+          <small>Coded by Caroline Månsson</small>
+        </footer>
       </div>
     </div>
   );
