5fe47a8c608517370a8a6ac215d07ade97c1789c
carolinemansson <97236288+carolinemansson@users.noreply.github.com>
2022-07-05 14:32:12 +0200

    Added background img

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import Dictionary from "./Dictionary";
 
-
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="App-header"><span className="dino">DINO</span> Dictionary</header>
         <main>
-          <Dictionary defaultKeyword="Dinosaur" />
+          <Dictionary defaultKeyword="dinosaur" />
         </main>
         <footer className="App-footer"><small>Coded by Caroline Månsson</small></footer>
       </div>
